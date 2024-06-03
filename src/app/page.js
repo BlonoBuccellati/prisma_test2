5c325b76514db865160ee74d87fd68aabb55915b
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2024-06-03 20:24:23 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Main from './components/todoMain';
 export default function Home() {
-  useState();
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Main props={val} />
